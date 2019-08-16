2384a25e08e581133c8565a532cb9bbf6b35274d
akackon <akackon@localhost.localdomain>
2019-08-16 02:34:53 +0100

    tried a little something out with pandas

---
+++ usingPandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df = pd.read_csv("HR_comma_sep.csv")
+
+print(type(df))
+print (df.shape)
+print(df.columns)
+
+print(df.head(5))
+print(df.tail(5))
+print(df.info())
+
+print(df.describe())
+
+print(df["satisfaction_level"])
+df2 = df[["satisfaction_level","salary","sales"]]
+print(df2)
