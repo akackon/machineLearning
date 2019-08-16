0131eb2799a021283d1186cd8af2676981588856
akackon <akackon@localhost.localdomain>
2019-08-16 01:23:58 +0100

    Using udemy course

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Machine Learning
 # This tutorial is being done with the aid of the Python for Probability, Statistics and Machine Learning by
 # Jose Unpingco
+
+# I'm now using the Udemy course on Machine Learning
---
+++ dataTypes.py
@@ -0,0 +1,33 @@
+a = 101
+print(a)
+type(a)
+
+a = a+6
+a = a-890
+
+a = a/12
+
+flag = bool()
+
+flag = True
+
+# String
+b = "My name is XYZ"
+
+b.title()
+
+c = b +" " + "and I am cool"
+print(c)
+
+# List, Tuple, Set and Dictionary
+list1 = [1,2,3,4,5]
+print(list1[0])
+
+list2 = [3,4,5,6,80]
+list3 = list1 + list2
+print(list3)
+
+print(len(list3))
+
+#tuple
+# They do not allow you to change their values
