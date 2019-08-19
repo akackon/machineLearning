ca7673adf84c2b2ac3d829d7ed205b4cc94c8679
akackon <akackon@localhost.localdomain>
2019-08-19 20:17:07 +0100

    Done working with some line charts

--- visualization.py
+++ visualization.py
@@ -1,29 +1,44 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.random.randint(100,500,50,)
-y = np.random.randint(100,1000,50,)
+# x = np.random.randint(100,500,50,)
+# y = np.random.randint(100,1000,50,)
 
-print(x)
-print(y)
+# print(x)
+# print(y)
 
-plt.scatter(x,y, marker = "^",color = "red")
-plt.scatter(y,x, marker = "^",color = "green")
-plt.title("This is a sample scatter plot")
-plt.xlabel("X axis label")
-plt.ylabel("Y axis label")
+# plt.scatter(x,y, marker = "^",color = "red")
+# plt.scatter(y,x, marker = "^",color = "green")
+# plt.title("This is a sample scatter plot")
+# plt.xlabel("X axis label")
+# plt.ylabel("Y axis label")
 # plt.show()
-# Commenting the above line because it makes the running of the code below long to get to
+# Commenting the above lines because it makes the running of the code below long to get to
 
 # Working with bar plots now
-x = np.arange(5)
-y = np.random.randint(50,100,5)
+# x = np.arange(5)
+# y = np.random.randint(50,100,5)
 
+# print(x)
+# print(y)
+# # xlist = list("ABCDE")
+# xlist = ["English", "Math", "History", "Science", "Geography"]
+# plt.title("Marks scored by John")
+# plt.xticks(x,xlist)
+# plt.bar(x,y)
+# plt.show()
+
+# Commenting the above lines because it makes the running of the code below long to get to
+
+x = np.arange(1,7,1)
+y = np.random.randint(50,100,6)
 print(x)
 print(y)
-# xlist = list("ABCDE")
-xlist = ["English", "Math", "History", "Science", "Geography"]
-plt.title("Marks scored by John")
+xlist = ["Sem1", "Sem2", "Sem3", "Sem4", "Sem5", "Sem6"]
 plt.xticks(x,xlist)
-plt.bar(x,y)
+plt.title("Overall College Performance")
+plt.ylabel("Percentage")
+plt.xlabel("Semesters")
+plt.plot(x,y,"b^")
+plt.plot(x,y,"red")
 plt.show()
