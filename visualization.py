2e8a7827388bae82bfe632a260462ec27a598eac
akackon <akackon@localhost.localdomain>
2019-08-19 20:08:26 +0100

    Worked with bar plots or bar charts

--- visualization.py
+++ visualization.py
@@ -12,4 +12,18 @@ plt.scatter(y,x, marker = "^",color = "green")
 plt.title("This is a sample scatter plot")
 plt.xlabel("X axis label")
 plt.ylabel("Y axis label")
+# plt.show()
+# Commenting the above line because it makes the running of the code below long to get to
+
+# Working with bar plots now
+x = np.arange(5)
+y = np.random.randint(50,100,5)
+
+print(x)
+print(y)
+# xlist = list("ABCDE")
+xlist = ["English", "Math", "History", "Science", "Geography"]
+plt.title("Marks scored by John")
+plt.xticks(x,xlist)
+plt.bar(x,y)
 plt.show()
