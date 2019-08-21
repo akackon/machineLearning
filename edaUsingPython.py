1b27d1a9ffc9d32f0da00d9275008ee59226783a
akackon <akackon@localhost.localdomain>
2019-08-21 04:52:22 +0100

    done exploring some functions of bivariate data analysis

---
+++ edaUsingPython.py
@@ -0,0 +1,29 @@
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
+df = pd.read_csv("HR_comma_sep.csv")
+print(df.describe())
+
+print(df["satisfaction_level"].describe())
+print(df["satisfaction_level"].plot(kind = "box"))
+
+plt.boxplot(df["satisfaction_level"], showmeans = True)
+plt.show()
+
+df["satisfaction_level"].plot(kind = "hist")
+
+plt.hist(df["satisfaction_level"], bins = 30)
+plt.show()
+
+print(df["satisfaction_level"].var())
+print(df["satisfaction_level"].std())
+print(df["satisfaction_level"].mean())
+print(df["satisfaction_level"].std()/df["satisfaction_level"].mean() * 100)
+print(df["satisfaction_level"].skew())
+print(df["satisfaction_level"].kurtosis())
+
+plt.show()
+
+print(df["sales"].unique())
+
+print(df["salary"].unique())
+pd.crosstab(df["sales"],df["salary"]).plot(kind = "bar", stacked = True)
+plt.show()
